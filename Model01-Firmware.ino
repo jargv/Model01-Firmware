e251642a907bd8541f02636bd450d3104f5ea4b9
Jonathan Geddes <geddes.jonathan@gmail.com>
2018-07-30 14:55:32 -0600

    More of my own keymap modifications

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -173,17 +173,17 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
-  (Key_Backtick,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (Key_Backtick,    Key_1, Key_2, Key_3, Key_4, Key_5, ___,
    Key_Tab,         Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_LeftShift,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_LeftControl, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftGui,
    Key_LeftAlt,     Key_Spacebar, Key_Enter, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   Key_RightGui, Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
-   Key_Enter,    Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                 Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_LEDEffectNext, Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Pipe,
+   Key_RightGui,      Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
+   Key_Enter,         Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                      Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_LEDEffectNext, Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Backslash,
    Key_RightShift, Key_LeftAlt, Key_Backspace, Key_Escape,
    ShiftToLayer(FUNCTION)),
 
